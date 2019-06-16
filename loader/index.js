4c99210620329657d99efb08114aa6017fdf1fdc
walker <hiniwoo@163.com>
2019-06-16 17:29:03 +0800

    使用sass-loader

--- loader/index.js
+++ loader/index.js
@@ -1,7 +1,13 @@
 import active from './active.jpg';
 import './index.css';
+import './index.scss';
+
+
 var root = document.getElementById('root');
 var img = new Image();
 img.src = active;
 img.classList.add('active');
+
+img.classList.add('active-scss');
+
 root.appendChild(img);
